fd9d8c0a6a588a373e8929a05cd0a8c791ebf28d
Koober97 <koober@Brians-MacBook-Pro.local>
2022-11-08 03:49:05 -0500

    created all the question arrays

--- index.js
+++ index.js
@@ -1,7 +1,68 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateReadMe = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "Title",
+        type: "Input",
+        message: "What is your Application Title:"
+    },
+    {
+        name: "Name",
+        type: "Input",
+        message: "Name:"
+    },
+    {
+        name: "Email",
+        type: "Input",
+        message: "Email Address:"
+    },
+    {
+        name: "Github",
+        type: "Input",
+        message: "Github Username:"
+    },
+    {
+        name: "Description",
+        input: "Input",
+        message: "Describe the Application:"
+    },
+    {
+        name: "Tech",
+        type: "Checkbox",
+        choices: ["HTML", "CSS", "JavaScript", "Bootstrap", "jQuery", "Node.js", "React.js"],
+        message: "Which tech did you use to build this:"
+    },
+    {
+        name: "Installation",
+        input: "Input",
+        message: "Describe the Installation Process:"
+    },
+    {
+        name: "Usage",
+        type: "Input",
+        message: "What is the use case:"
+    },
+    {
+        name: "Contributing",
+        type: "Input",
+        message: "Project Contribution Guidelines:"
+    },
+    {
+        name: "Tests",
+        type: "Input",
+        message: "Testing Instructions:"
+    },
+    {
+        name: "License",
+        type: "Checkbox",
+        message: "Which licese/license's will it use:",
+        choices: ["MIT", "BSD", "Apache", "GPL", "ISC", "Public", "None"],
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
